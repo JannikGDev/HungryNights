f4e1425a0bf6a2829f68c0a06abf8df314750073
JannikG <sodacookiedev@gmail.com>
2019-09-14 16:41:03 +0200

    Added Digger

--- Scenes/Person/Person.gd
+++ Scenes/Person/Person.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 # Declare member variables here. Examples:
-export var baseSpeed = 300
+export var speed = 300
 
 onready var anim: AnimationPlayer = get_node("AnimationPlayer")
 onready var polygons: Node2D = get_node("polygons")
@@ -12,56 +12,10 @@ var attacking: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	anim.playback_speed = baseSpeed / 300
-	
-	pass # Replace with function body.
+	anim.play("walk")
+	anim.playback_speed = speed / 300
 
-
-func movement(delta):
-	
-	move = Vector2(0,0)
-	
-	if Input.is_action_pressed("move_down"):
-		move += Vector2(0,1)
-		
-	if Input.is_action_pressed("move_up"):
-		move += Vector2(0,-1)
-		
-	if Input.is_action_pressed("move_right"):
-		move += Vector2(1,0)
-		
-	if Input.is_action_pressed("move_left"):
-		move += Vector2(-1,0)
-		
-	if move.length() > 0:
-		anim.play("walk")
-	else:
-		anim.play("idle")
-		
-	if move.x > 0:
-		polygons.scale.x = 1
-		skeleton.scale.x = 1
-	elif move.x < 0:
-		polygons.scale.x = -1
-		skeleton.scale.x = -1
-		
-	move_and_collide(baseSpeed*move*delta*abs(scale.x))
-
-func attackCheck(delta):
-	if Input.is_action_just_pressed("attack") and !attacking:
-		anim.play("attack")
-		attacking = true
-		
-	if attacking and (anim.current_animation != "attack" or !anim.is_playing()):
-		attacking = false
 
 func _process(delta):
-	if !attacking:
-		movement(delta)
-	
-	attackCheck(delta)
-	
-	
-	move_and_collide(baseSpeed*move*delta)
+	pass
 
