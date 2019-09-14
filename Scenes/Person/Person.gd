ac7524a257338cc655913d873dc52aa8f85cb293
JannikG <sodacookiedev@gmail.com>
2019-09-14 14:49:16 +0200

    Improved Player Movement

--- Scenes/Person/Person.gd
+++ Scenes/Person/Person.gd
@@ -1,19 +1,24 @@
 extends KinematicBody2D
 
 # Declare member variables here. Examples:
-const baseSpeed = 300
+export var baseSpeed = 300
 
 onready var anim: AnimationPlayer = get_node("AnimationPlayer")
 onready var polygons: Node2D = get_node("polygons")
 onready var skeleton: Node2D = get_node("Skeleton2D")
 var move = Vector2()
 
+var attacking: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	anim.playback_speed = baseSpeed / 300
+	
 	pass # Replace with function body.
 
 
-func _process(delta):
+func movement(delta):
 	
 	move = Vector2(0,0)
 	
@@ -41,6 +46,21 @@ func _process(delta):
 		polygons.scale.x = -1
 		skeleton.scale.x = -1
 		
+	move_and_collide(baseSpeed*move*delta*abs(scale.x))
+
+func attackCheck(delta):
+	if Input.is_action_just_pressed("attack") and !attacking:
+		anim.play("attack")
+		attacking = true
 		
-	move_and_collide(baseSpeed*move*delta)
-		
+	if attacking and (anim.current_animation != "attack" or !anim.is_playing()):
+		attacking = false
+
+func _process(delta):
+	if !attacking:
+		movement(delta)
+	
+	attackCheck(delta)
+	
+	
+
