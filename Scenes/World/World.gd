8f551343484418e7bc29a7b897fdda34a53a09b5
noize21 <lukas.seltmann@live.de>
2019-09-15 14:42:08 +0200

    make house interior appear when walking in

--- Scenes/World/World.gd
+++ Scenes/World/World.gd
@@ -12,3 +12,13 @@ func _on_vampire_player_attacked():
 	$Camera2D.global_position = $Player.global_position
 	$Camera2D.current = true
 	add_child(smoke_instance)
+
+
+func _on_Area2D_body_entered(body):
+	if body.get_name() == "Player":
+		get_node("WorldEnvironment/HousesOutside").visible = false
+
+
+func _on_Area2D_body_exited(body):
+	if body.get_name() == "Player":
+		get_node("WorldEnvironment/HousesOutside").visible = true
