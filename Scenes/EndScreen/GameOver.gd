cdb653de5c1b294739030f29973ca5d4fcf2017e
Firstus <Firstus@web.de>
2019-09-15 15:27:32 +0200

    Add gameover screen

---
+++ Scenes/EndScreen/GameOver.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _process(delta):
+	var state = get_node("/root/GameState")
+	
+	if state.is_game_over:
+		if state.game_over_state == state.game_over.KILLED:
+			$TypicalGameOver.visible = true
+			$LotGameOvrer.visible = false
+		if state.game_over_state == state.game_over.BURNBYLO:
+			$TypicalGameOver.visible = false
+			$LotGameOvrer.visible = true
+		if Input.is_action_pressed("ui_accept"):
+			get_tree().change_scene("res://StartScreen/StartScreen.tscn")
