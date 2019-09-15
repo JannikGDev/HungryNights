e4d34098de24b4a3b2ed8e3c8cc7c41b4426cbd4
Firstus <Firstus@web.de>
2019-09-15 14:27:58 +0200

    modify ui and add lantern

--- Scenes/Ui/Control.gd
+++ Scenes/Ui/Control.gd
@@ -1,13 +1,5 @@
 extends Control
-
-func set_time(time: float):
-	$Clock/Pointer.rotate(time / 6 * PI)
-
-func get_time():
-	return ($Clock/Pointer.rotation / PI) * 6 
 	
-func set_attention(attention: float):
-	$Attention.value = attention
-
-func get_attention():
-	return $Attention.value
+func _physics_process(delta):
+	$Attention.value = get_node("/root/GameState").attention
+	$Control/Clock/Pointer.rotation = (fmod(get_node("/root/GameState").daytime, 12) * ( (2 * PI) / 12))
